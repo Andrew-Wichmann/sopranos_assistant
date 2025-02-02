30ed833195dc1ff837ecb140aa07b3a1ebfb4cfb
Andrew Wichmann <andrewp.wichmann@gmail.com>
2025-02-02 18:12:51 -0500

    feat: add basic Alibaba chat component

--- src/App.tsx
+++ src/App.tsx
@@ -1,44 +1,31 @@
-import { useEffect, useState } from "react";
-import type { Schema } from "../amplify/data/resource";
-import { generateClient } from "aws-amplify/data";
+import Chat, { useMessages, Bubble, MessageProps } from "@chatui/core";
 import { useAuthenticator } from "@aws-amplify/ui-react";
-
-const client = generateClient<Schema>();
+import '@chatui/core/es/styles/index.less';
+import '@chatui/core/dist/index.css';
 
 function App() {
-  const [todos, setTodos] = useState<Array<Schema["Todo"]["type"]>>([]);
   const { signOut, user } = useAuthenticator()
+  const { messages, appendMsg } = useMessages();
 
-  function deleteTodo(id: string) {
-    client.models.Todo.delete({ id });
+  function renderMessage(msg: MessageProps) {
+      const { content } = msg
+      return <Bubble content={content.text} />
   }
 
-  useEffect(() => {
-    client.models.Todo.observeQuery().subscribe({
-      next: (data) => setTodos([...data.items]),
-    });
-  }, []);
-
-  function createTodo() {
-    client.models.Todo.create({ content: window.prompt("Todo content") });
+  function handleSend(type: any, val: string) {
+      if (type === "text" && val.trim()) {
+          appendMsg({
+              type: "text",
+              content: {text: val},
+              position: "right",
+          })
+      }
   }
 
   return (
     <main>
-      <h1>{user?.signInDetails?.loginId}'s todos</h1>
-      <button onClick={createTodo}>+ new</button>
-      <ul>
-        {todos.map((todo) => (
-          <li onClick={() => deleteTodo(todo.id)} key={todo.id}>{todo.content}</li>
-        ))}
-      </ul>
-      <div>
-        🥳 App successfully hosted. Try creating a new todo.
-        <br />
-        <a href="https://docs.amplify.aws/react/start/quickstart/#make-frontend-updates">
-          Review next step of this tutorial.
-        </a>
-      </div>
+      <h1>{user?.signInDetails?.loginId} welcome!</h1>
+        <Chat messages={messages} renderMessageContent={renderMessage} onSend={handleSend}/>
       <button onClick={() => signOut()}>Sign out</button>
     </main>
   );
